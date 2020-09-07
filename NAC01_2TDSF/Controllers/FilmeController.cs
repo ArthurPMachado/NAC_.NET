c399d0d3fe9c71eb8a08d2e9fac5d25c500cf3b6
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-07 16:39:01 -0300

    Feat: create index view and filme list

---
+++ NAC01_2TDSF/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NAC01_2TDSF.Models;
+
+namespace NAC01_2TDSF.Controllers
+{
+    public class FilmeController : Controller
+    {
+        private static List<Filme> _banco = new List<Filme>();
+
+        // Home page dos filmes
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
