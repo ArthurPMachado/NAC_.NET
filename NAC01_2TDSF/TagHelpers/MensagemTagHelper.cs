4810626fdc74f9d0adc8a83c9ee3558e31d7ff8c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-07 17:32:30 -0300

    Feat: create tag helper

---
+++ NAC01_2TDSF/TagHelpers/MensagemTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NAC01_2TDSF.TagHelpers
+{
+    public class MensagemTagHelper : TagHelper
+    {
+        public string Validacao { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.Attributes.SetAttribute("class", "alert alert-success");
+            output.Content.SetContent(Validacao);
+        }
+    }
+}
